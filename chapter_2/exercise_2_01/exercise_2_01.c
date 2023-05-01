26395c8a8afab61e73e2a2db30c3cde2b5a35633
magicmorz <60649690+magicmorz@users.noreply.github.com>
2023-05-01 23:13:43 +0300

    chapter 2 lets go

---
+++ chapter_2/exercise_2_01/exercise_2_01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+main()
+{
+    printf("value of CHAR_BIT %d\n", CHAR_BIT);
+    printf("value of CHAR_MAX %d\n", CHAR_MAX);
+    printf("value of UCHAR_MAX %d\n", UCHAR_MAX);
+    printf("value of SCHAR_MAX %d\n", SCHAR_MAX);
+    printf("value of CHAR_MIN %d\n", CHAR_MIN);
+    printf("value of SCHAR_MIN %d\n", SCHAR_MIN);
+    printf("value of INT_MAX %d\n", INT_MAX);
+    printf("value of INT_MIN %d\n", INT_MIN);
+    printf("value of LONG_MAX %d\n", LONG_MAX);
+    printf("value of LONG_MIN %d\n", LONG_MIN);
+    printf("value of SCHAR_MAX %d\n", SCHAR_MAX);
+    printf("value of SCHAR_MIN %d\n", SCHAR_MIN);
+    printf("value of SHRT_MAX %d\n", SHRT_MAX);
+    printf("value of SHRT_MIN %d\n", SHRT_MIN);
+    printf("value of UCHAR_MAX %d\n", UCHAR_MAX);
+    printf("value of UINT_MAX %d\n", UINT_MAX);
+    printf("value of ULONG_MAX %d\n", ULONG_MAX);
+    printf("value of USHRT_MAX %d\n", USHRT_MAX);
+}
