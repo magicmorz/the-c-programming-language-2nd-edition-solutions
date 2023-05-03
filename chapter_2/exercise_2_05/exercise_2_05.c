3e4e61a6f2517a11763fcd2218f59a48f6584a99
magicmorz <60649690+magicmorz@users.noreply.github.com>
2023-05-03 07:43:15 +0300

    another one

---
+++ chapter_2/exercise_2_05/exercise_2_05.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define STRING_MAX_SIZE 100
+
+int any(char s1[], char s2[]);
+
+int main()
+{
+
+    char s1[STRING_MAX_SIZE];
+    char s2[STRING_MAX_SIZE];
+    printf("enter string s1\n");
+    get_string(s1);
+    printf("enter string s2\n");
+    get_string(s2);
+    printf("%d\n", any(s1, s2));
+    return 0;
+}
+
+int any(char s1[], char s2[])
+{
+    int i, j;
+    int found = 0;
+    for (i = j = 0; s1[i] != '\0'; i++)
+    {
+        for (int k = 0; (s2[k] != '\0') && !found; k++)
+        {
+            if (s1[i] == s2[k])
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int get_string(char s[])
+{
+    int c;
+    int i = 0;
+    while ((c = getchar()) != EOF)
+    {
+        s[i] = c;
+        i++;
+    }
+    s[i] = '\0';
+    return i;
+}
