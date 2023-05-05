89d06e1ea912edae675fa1ad39131c4e20ea26b1
magicmorz <60649690+magicmorz@users.noreply.github.com>
2023-05-05 23:34:32 +0300

    i am bitwise champion

---
+++ chapter_2/exercise_2_06/exercise_2_06.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+unsigned getbits(unsigned x, int p, int n);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+int main()
+{
+    printf("%d\n",setbits(0,6,3, 60));
+    return 0;
+}
+
+/* getbits: get n bits from position p*/
+unsigned getbits(unsigned x, int p, int n)
+{
+    return (x>>(p+1-n))&~(~0<<n);
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+    x = getbits(y, p, n);
+    y = y >> n;
+    y = y << n;
+    y = y |x;
+    return y;
+}
