eeea72a7518cdb8b3a9a1002385d8a5377ff400a
magicmorz <60649690+magicmorz@users.noreply.github.com>
2023-06-10 03:01:05 +0300

    ok this is a 'little' better

--- chapter_4/exercise_4_13/exercise_4_13.c
+++ chapter_4/exercise_4_13/exercise_4_13.c
@@ -6,26 +6,23 @@ int main()
     int num = 5894;
     char s[MAXSIZE] = "abcdefg";
 
-    char d[MAXSIZE] = "       ";
-
-    reverse(s, d, strlen(s)-1, 0);
-    printf("%s\n", d);
+   
+    reverse(s);
+    printf("%s\n", s);
     return 0;
 }
 
-int reverse(char s[], char d[], int i, int j)
-{
-    if (j == strlen(d))
-    {
-        return 0;
-    }
-    if (s[i + 1] == '\0')
-    {
-        d[j] = s[i];
-        s[i] = '\0';
-        i--;
-        j++;
-    }
 
-    reverse(s, d, i, j);
+int reverse (char s[])
+{
+    static int i;
+    static int j;
+    int c;
+    if (s[i]!='\0')
+    {
+        c = s[i++];
+        reverse(s);
+        s[j++]= c;
+    }
+    
 }
