cc6e213490a4cf893af32d66dcae9287bbd900aa
magicmorz <60649690+magicmorz@users.noreply.github.com>
2023-06-11 07:42:14 +0300

    this is what they meant

--- chapter_4/exercise_4_14/exercise_4_14.c
+++ chapter_4/exercise_4_14/exercise_4_14.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#define swap(t, x, y) {\
-    t = x;\
-    x = y;\
-    y = t;\
-}
+#define swap(t, x, y) \
+    {                 \
+        t temp;       \
+        temp = x;     \
+        x = y;        \
+        y = temp;     \
+    };
 int main(int argc, char const *argv[])
 {
-    int a = 3, b = 4, t;
-    swap(t, a, b);
+    int a = 3, b = 4;
+    swap(int, a, b);
     printf("a = %d, b = %d \n", a, b);
     return 0;
 }
