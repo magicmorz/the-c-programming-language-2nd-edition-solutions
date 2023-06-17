87dbc20dba20ad2e4a6714914580668c8cde6e40
magicmorz <60649690+magicmorz@users.noreply.github.com>
2023-06-17 11:33:52 +0300

    strindex (is it considered cheating tho)

--- chapter_5/exercise_5_6/strindex.c
+++ chapter_5/exercise_5_6/strindex.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
+
+char* pattern = "ould";
 int main()
 {
     char s[40];
-    while (*s!=EOF)
+    int n;
+    while (*s != EOF)
     {
         getline1(s);
-        printf("%s\n",s);
+        n = strindex(s, pattern);
+        if (n >= 0)
+        {
+            printf("%d\n", n);
+            printf("%s\n", s);
+        }
     }
-    
+
     return 0;
 }
 
 void getline1(char *s)
 {
-    while (((*s = getchar())!= '\n')&&(*s++!=EOF))
-    ;
+    while (((*s = getchar()) != '\n') && (*s++ != EOF))
+        ;
     *s = '\0';
+}
+
+int strindex(char* s, char* t)
+{
+    int i, j, k;
     
+    
+    for (i = 0; *(s+i) != '\0'; i++)
+    {
+        for (j = i, k = 0; *(t+k) != '\0' && *(s+j) == *(t+k); j++, k++)
+            ;
+        if (k > 0 && *(t+k) == '\0')
+        {
+            return i;
+        }
+    }
+    return -1;
 }
