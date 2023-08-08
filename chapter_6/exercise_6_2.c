dfb4d9111ca965109018bbf353fa6cd107a33a21
magicmorz <60649690+magicmorz@users.noreply.github.com>
2023-08-08 07:52:37 +0300

    got all variable names

--- chapter_6/exercise_6_2.c
+++ chapter_6/exercise_6_2.c
@@ -10,7 +10,7 @@ struct key
     int count;
 } keytab[] = {
     {"auto", 0}, {"break", 0}, {"case", 0}, {"char", 0}, {"const", 0}, {"continue", 0}, {"default", 0}, {"double", 0}, {"else", 0}, {"enum", 0}, {"extern", 0}, {"float", 0}, {"for", 0}, {"goto", 0}, {"if", 0}, {"int", 0}, {"long", 0}, {"register", 0}, {"return", 0}, {"short", 0}, {"signed", 0}, {"sizeof", 0}, {"static", 0}, {"struct", 0}, {"switch", 0}, {"typedef", 0}, {"union", 0}, {"unsigned", 0}, {"void", 0}, {"volatile", 0}, {"while", 0}};
-
+int keytab_len = sizeof(keytab) / sizeof(keytab[0]);
 char *variable_types[] = {"int", "long", "short", "double", "char"};
 
 #define NKEYS (sizeof keytab / sizeof keytab[0])
@@ -23,32 +23,57 @@ main()
     int n;
     char word[MAXWORD];
     int i = 0;
+    int c;
+    int is_saved_word = 0;
     while (getword(word, MAXWORD) != EOF)
     {
         if (isalpha(word[0]))
         {
             if ((n = binsearch(word, keytab, NKEYS)) >= 0)
             {
-                if (strcmp(word, variable_types[0])==0)
+                if (string_in_string_array(word, variable_types, (sizeof(variable_types) / sizeof(variable_types[0]))) >= 0)
                 {
-                    if (getword(word, MAXWORD) != EOF)
+                    do
                     {
-                        if (isalpha(word[0]))
+                        while ((isspace(c = getc(stdin))) || (c == '*'))
+                            ;
+
+                        ungetc(c, stdin);
+
+                        if (getword(word, MAXWORD) != EOF)
                         {
-                            // Allocate memory for each word before copying
-                            variable_names[i] = (char *)malloc(strlen(word) + 1);
-                            if (variable_names[i] != NULL)
+                            if (!((c = getc(stdin)) == '('))
                             {
-                                strcpy(variable_names[i], word);
-                                i++;
+                                if (isalpha(word[0]))
+                                {
+                                    for (int i = 0; i < keytab_len; i++)
+                                    {
+                                        if (strcmp(word, keytab[i].word) == 0)
+                                        {
+                                            is_saved_word = 1;
+                                            break;
+                                        }
+                                    }
+                                    if (is_saved_word == 0)
+                                    {
+                                        // Allocate memory for each word before copying
+                                        variable_names[i] = (char *)malloc(strlen(word) + 1);
+                                        if (variable_names[i] != NULL)
+                                        {
+                                            strcpy(variable_names[i], word);
+                                            i++;
+                                        }
+                                        else
+                                        {
+                                            // Handle memory allocation failure, e.g., print an error message
+                                            printf("Memory allocation failed for word: %s\n", word);
+                                        }
+                                    }
+                                }
                             }
-                            else
-                            {
-                                // Handle memory allocation failure, e.g., print an error message
-                                printf("Memory allocation failed for word: %s\n", word);
-                            }
+                            ungetc(c, stdin);
                         }
-                    }
+                    } while ((getc(stdin)) == ',');
                 }
             }
         }
@@ -59,7 +84,17 @@ main()
 
     return 0;
 }
-
+int string_in_string_array(char *str, char *str_arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (strcmp(str, str_arr[i]) == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 int binsearch(char *word, struct key tab[], int n)
 {
     int cond;
