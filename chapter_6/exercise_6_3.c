a276016a66f5eacf61c23291192d00467fbed1c8
magicmorz <morozov101@gmail.com>
2023-08-11 00:22:38 +0300

    now it works yay

--- chapter_6/exercise_6_3.c
+++ chapter_6/exercise_6_3.c
@@ -5,12 +5,14 @@
 #define FILENAME "myfile.txt"
 #define MAX_WORD_LENGTH 30
 
-int getword(char *word, int lim, char *line);
+int getword(char *word, int lim, char **line_temp);
+int comment(char **line_temp);
 typedef struct
 {
     char *word;
     int *lines;
     int count;
+    int number;
 } word_info;
 
 int check_word_already_in_list(word_info *list, char *word);
@@ -21,9 +23,7 @@ int main(int argc, char const *argv[])
     char *line = NULL;
     size_t line_length = 0;
     int line_number = 0;
-     size = 1;
-    // = NULL;
-    word_info *word_list = (word_info *)malloc(sizeof(word_info) * size);
+    word_info *word_list = NULL;
     // Open a file in read mode
     FILE *fptr;
     fptr = fopen(FILENAME, "r");
@@ -43,36 +43,44 @@ int main(int argc, char const *argv[])
 
         char current_word[MAX_WORD_LENGTH];
         int pos;
-        
-        while (getword(current_word, MAX_WORD_LENGTH, line)) // loop over each word in the line
+
+        while (getword(current_word, MAX_WORD_LENGTH, &line)) // loop over each word in the line
         {
             // if this word already has a word_info object
             if ((pos = check_word_already_in_list(word_list, current_word)) != -1)
             {
                 // increament numner of appreances
                 (word_list + pos)->count++;
-                // make space for a new line number
-                (word_list + pos)->lines = (word_info *)realloc(word_list + pos, (word_list + pos)->count);
                 // save line number
-                (word_list + pos)->lines[(word_list + pos)->count] = line_number; // save line number
+                (word_list + pos)->lines[(word_list + pos)->count - 1] = line_number; // save line number
             }
             else // if not already created, create a word_info object for the word
             {
                 size++;
                 word_list = (word_info *)realloc(word_list, size * sizeof(word_info));
+                word_list[size - 1].number = size - 1;
                 // save word
-                word_list[size].word = (char*)malloc(strlen(current_word)*sizeof(char));
-                strcpy(word_list[size].word, current_word);
+                word_list[size - 1].word = (char *)malloc(strlen(current_word) * sizeof(char));
+                strcpy(word_list[size - 1].word, current_word);
                 // increament numner of appreances
-                word_list->count = 1;
-                // make space for a new line number
-                (word_list + size)->lines = (word_info *)realloc(word_list, (word_list + size)->count);
-                // save line number
-                (word_list + size)->lines[(word_list + size)->count] = line_number;
+                (word_list + size - 1)->count = 1;
+
+                word_list[size - 1].lines = (int *)malloc(sizeof(int));
+                // Assign line number to the last element of the lines array
+                word_list[size - 1].lines[word_list[size - 1].count - 1] = line_number;
             }
         }
-
         line_number++;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%s ", word_list[i].word);
+        for (int j = 0; j < word_list[i].count; j++)
+        {
+            printf("%d ", word_list[i].lines[j]);
+        }
+        putchar('\n');
     }
 
     return 0;
@@ -81,64 +89,66 @@ int main(int argc, char const *argv[])
 int check_word_already_in_list(word_info *list, char *word)
 {
     word_info *list_ptr = list;
-    if (size == 0) return -1;
+    if (list == NULL)
+        return -1;
     int i = 0;
-    while (list_ptr)
+    while (i < size)
     {
-        if (strcmp(list_ptr++->word, word) == 0)
+        if (strcmp(list_ptr->word, word) == 0)
         {
-            return i;
+            return list_ptr->number;
         }
         i++;
+        list_ptr++;
     }
     return -1;
 }
 
-int getword(char *word, int lim, char *line)
+int getword(char *word, int lim, char **line_temp)
 {
-    char *line_temp = line;
     int c, d;
     char *w = word;
 
-    while (isspace(c = *line_temp++))
+    while (isspace(c = *(*line_temp)++))
         ;
+
     if (c != EOF)
         *w++ = c;
+
     if (isalpha(c) || c == '_' || c == '#')
     {
         for (; --lim > 0; w++)
-            if (!isalnum(*w = getc(line_temp)) && *w != '_')
+        {
+            if (!isalnum(*w = *(*line_temp)++) && *w != '_')
             {
-                ungetc(*w, line_temp);
+                (*line_temp)--;
                 break;
             }
+        }
     }
     else if (c == '\'' || c == '"')
     {
         for (; --lim > 0; w++)
-            if ((*w = getc(line_temp)) == '\\')
+        {
+            if ((*w = *(*line_temp)++) == '\\')
             {
-                *++w = getc(line_temp);
+                *++w = *(*line_temp)++;
             }
             else if (*w == c)
             {
-                w++;
                 break;
             }
-            else if (*w == EOF)
-            {
-                break;
-            }
+        }
     }
     else if (c == '/')
     {
-        if ((d = getc(line_temp)) == '*')
+        if ((d = *(*line_temp)++) == '*')
         {
             c = comment(line_temp);
         }
         else
         {
-            ungetc(d, line_temp);
+            (*line_temp)--;
         }
     }
     else if (c == EOF)
@@ -150,20 +160,20 @@ int getword(char *word, int lim, char *line)
     return word[0];
 }
 
-int comment(FILE *fptr)
+int comment(char **line_temp)
 {
     int c;
-    while ((c = getc(fptr)) != EOF)
+    while ((c = *(*line_temp)++) != EOF)
     {
         if (c == '*')
         {
-            if ((c = getc(fptr)) == '/')
+            if ((c = *(*line_temp)++) == '/')
             {
                 break;
             }
             else
             {
-                ungetc(c, fptr);
+                (*line_temp)--;
             }
         }
     }
