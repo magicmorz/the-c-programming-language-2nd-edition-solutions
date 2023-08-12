88fc89b27488e12e7192a4cfde391bed083ed687
magicmorz <morozov101@gmail.com>
2023-08-12 21:30:31 +0300

    nice

--- chapter_6/exercise_6_6/exercise_6_6.c
+++ chapter_6/exercise_6_6/exercise_6_6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define HASHSIZE 101
+#define MAX_WORD_LENGTH 30
 static struct nlist *hashtab[HASHSIZE];
 unsigned hash(char *s);
 struct nlist *lookup(char *s);
@@ -14,7 +15,28 @@ struct nlist
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    char current_word[MAX_WORD_LENGTH];
+    char c;
+    char prev = NULL;
+
+    char *name = (char*)malloc(sizeof(char));
+    char *defn = (char*)malloc(sizeof(char));
+    while ((c = getc(stdin))!=EOF)
+    {
+        if ((c == '#') && ((prev == NULL) || (prev == '\n')))
+        {
+            getword(current_word, MAX_WORD_LENGTH);
+            if (strcmp(current_word, "define") == 0)
+            {
+                getword(name, MAX_WORD_LENGTH);
+                getword(defn, MAX_WORD_LENGTH);
+                install(name,defn);
+            }
+        }
+
+        prev = c;
+    }
+
     return 0;
 }
 
@@ -87,3 +109,99 @@ struct nlist *install(char *name, char *defn)
     return np;
 }
 
+int getword(char *word, int lim)
+{
+    int c, d;
+    char *w = word;
+
+    while (isspace(c = getc(stdin)))
+        ;
+    if (c != EOF)
+        *w++ = c;
+    if (isalpha(c) || c == '_' || c == '#')
+    {
+        for (; --lim > 0; w++)
+            if (!isalnum(*w = getc(stdin)) && *w != '_')
+            {
+                ungetc(*w, stdin);
+                break;
+            }
+    }
+    else if (c == '\'' || c == '"')
+    {
+        for (; --lim > 0; w++)
+            if ((*w = getc(stdin)) == '\\')
+            {
+                *++w = getc(stdin);
+            }
+            else if (*w == c)
+            {
+                w++;
+                break;
+            }
+            else if (*w == EOF)
+            {
+                break;
+            }
+    }
+    else if (c == '/')
+    {
+        if ((d = getc(stdin)) == '*')
+        {
+            c = block_comment();
+        }
+
+        else
+        {
+            ungetc(d, stdin);
+        }
+        if ((d = getc(stdin)) == '/')
+        {
+            c = line_comment();
+        }
+        else
+        {
+            ungetc(d, stdin);
+        }
+    }
+    else if (c == EOF)
+    {
+        return EOF;
+    }
+
+    *w = '\0';
+    return word[0];
+}
+
+int block_comment(void)
+{
+    int c;
+    while ((c = getc(stdin)) != EOF)
+    {
+        if (c == '*')
+        {
+            if ((c = getc(stdin)) == '/')
+            {
+                break;
+            }
+            else
+            {
+                ungetc(c, stdin);
+            }
+        }
+    }
+    return c;
+}
+
+int line_comment(void)
+{
+    int c;
+    while ((c = getc(stdin)) != EOF)
+    {
+        if (c == '\n')
+        {
+            break;
+        }
+    }
+    return c;
+}
